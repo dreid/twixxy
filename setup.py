62cecaaff0f19f5824515826fffbfd0bba4d9fbf
David Reid <dreid@dreid.org>
2013-02-12 12:38:52 -0800

    Add twixxy.features to packages and bump version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('README.rst') as readme:
 
 setup(
     name='twixxy',
-    version='0.1.0',
+    version='0.1.1',
     description='Twisted integration with the twiggy logging library.',
     url='https://github.com/dreid/twixxy',
     author='David Reid',
     author_email='dreid@dreid.org',
-    packages=['twixxy'],
+    packages=['twixxy', 'twixxy.features'],
     license='MIT',
     long_description=long_description,
     install_requires=['Twisted', 'twiggy']
